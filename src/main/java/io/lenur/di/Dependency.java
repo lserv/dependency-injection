61c8387fa978265da601fdc9e173e9faea49151b
lenur <lenur.serveriev@gmail.com>
2020-05-06 11:18:58 +0300

    added Dependency class

---
+++ src/main/java/io/lenur/di/Dependency.java
@@ -0,0 +1,28 @@
+package io.lenur.di;
+
+import io.lenur.di.util.FileScanner;
+import io.lenur.di.util.Injector;
+import io.lenur.di.util.Processor;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+
+public class Dependency {
+    private static Map<Class<?>, Object> instances = new HashMap<>();
+    private static final FileScanner scanner = new FileScanner();
+    private static final Processor processor = new Processor();
+    private static final Injector injector = new Injector();
+
+    public static void init(String packageName) {
+        if (instances.size() == 0) {
+            final List<Class<?>> classes = scanner.getClasses(packageName);
+            processor.process(classes);
+            instances = processor.getInstances();
+        }
+    }
+
+    public static Object getInstance(Class<?> clazz) {
+        return injector.inject(clazz, instances);
+    }
+}
